aae949c2d77553b4583161b8d58ed1151fdd0b79
나경호 <hoyana1225@gmail.com>
2024-06-21 02:31:07 +0900

    chore: cicd 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,30 @@
 #!/usr/bin/env bash
 
+# 환경 설정
+DOCKER_IMAGE=${1}
+DOCKER_TAG=${2}
+DOCKER_CONTAINER_NAME="maru-egg-dev"
 PROJECT_ROOT="/home/ubuntu/app"
+DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
-# Stop the currently running application
-/home/ubuntu/app/stop.sh
+TIME_NOW=$(date +%c)
 
-# Copy the new jar file
-cp $PROJECT_ROOT/build/libs/*.jar $PROJECT_ROOT/MARU_EGG_BE.jar
+# 로그 출력
+echo "$TIME_NOW > 배포 스크립트 실행" >> $DEPLOY_LOG
 
-# Start the application
-/home/ubuntu/app/start.sh
+# 기존 컨테이너 중지 및 삭제
+echo "$TIME_NOW > 기존 컨테이너 중지 및 삭제" >> $DEPLOY_LOG
+sudo docker stop $DOCKER_CONTAINER_NAME || true
+sudo docker rm $DOCKER_CONTAINER_NAME || true
+
+# Docker 이미지 Pull
+echo "$TIME_NOW > Docker 이미지 풀링: $DOCKER_IMAGE:$DOCKER_TAG" >> $DEPLOY_LOG
+sudo docker pull $DOCKER_IMAGE:$DOCKER_TAG
+
+# 새로운 컨테이너 실행
+echo "$TIME_NOW > 새로운 컨테이너 실행" >> $DEPLOY_LOG
+sudo docker run -d --log-driver=syslog --name $DOCKER_CONTAINER_NAME -p 8080:8080 $DOCKER_IMAGE:$DOCKER_TAG
+
+# 불필요한 이미지 삭제
+echo "$TIME_NOW > 불필요한 Docker 이미지 삭제" >> $DEPLOY_LOG
+sudo docker image prune -f
