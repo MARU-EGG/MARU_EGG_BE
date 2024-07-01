b8d48c99078b9ecf80293ca18d62e8d58c61a593
나경호 <hoyana1225@gmail.com>
2024-07-01 22:07:26 +0900

    test: test 환경 설정

--- src/test/java/mju/iphak/maru_egg/MaruEggApplicationTests.java
+++ src/test/java/mju/iphak/maru_egg/MaruEggApplicationTests.java
@@ -3,9 +3,11 @@ package mju.iphak.maru_egg;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
+@ActiveProfiles("test")
 class MaruEggApplicationTests {
 
 	@Test
